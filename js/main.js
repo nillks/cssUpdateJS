484c28052791ff6aa9a3918e6065db0a3d76b746
nillks <daniil.troanov2@gmail..com>
2024-02-07 22:18:38 +0600

    feat: сделал логику изменения css переменных

--- js/main.js
+++ js/main.js
@@ -1,4 +1,22 @@
-var фыв = 1;
-function ME() {
-  console.log(kek);
+let input = document.querySelectorAll("input");
+let inputArr = Array.from(input);
+
+
+function changeInput() {
+  switch (this.name) {
+    case "color":
+      document.documentElement.style.setProperty("--var-color", this.value);
+      break;
+    case "blur":
+      document.documentElement.style.setProperty("--var-blur", `blur(${this.value}px)`);
+      break;
+    case "padding":
+      document.documentElement.style.setProperty("--var-padding", this.value + 'px');
+      break;
+    default:
+      break;
+  }
 }
+
+
+inputArr.map((item) => item.addEventListener("change", changeInput));
