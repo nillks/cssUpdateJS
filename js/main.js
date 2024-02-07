42e9698bb7a6849852a725ea9649e53ddc79152e
nillks <daniil.troanov2@gmail..com>
2024-02-07 22:39:38 +0600

    refactor: сделал рефакторинг кода

--- js/main.js
+++ js/main.js
@@ -1,22 +1,39 @@
-let input = document.querySelectorAll("input");
-let inputArr = Array.from(input);
+// Создаем объект для управления CSS переменными
+const cssVariablesManager = {
+  set: function (name, value) {
+    document.documentElement.style.setProperty(`--var-${name}`, value);
+  },
+};
 
-
+// Функция для изменения CSS переменных
 function changeInput() {
-  switch (this.name) {
-    case "color":
-      document.documentElement.style.setProperty("--var-color", this.value);
-      break;
-    case "blur":
-      document.documentElement.style.setProperty("--var-blur", `blur(${this.value}px)`);
-      break;
-    case "padding":
-      document.documentElement.style.setProperty("--var-padding", this.value + 'px');
-      break;
-    default:
-      break;
+  // Проверяем, что объект this определен и имеет свойства name и value
+  if (this && this.name && this.value) {
+    switch (this.name) {
+      case "color":
+        cssVariablesManager.set("color", this.value);
+        break;
+      case "blur":
+        // Проверяем, что введенное значение является числом
+        if (!isNaN(parseFloat(this.value))) {
+          cssVariablesManager.set("blur", `blur(${this.value}px)`);
+        }
+        break;
+      case "padding":
+        // Проверяем, что введенное значение является числом
+        if (!isNaN(parseFloat(this.value))) {
+          cssVariablesManager.set("padding", `${this.value}px`);
+        }
+        break;
+      default:
+        break;
+    }
   }
 }
 
-
-inputArr.map((item) => item.addEventListener("change", changeInput));
+// Добавляем один обработчик событий на общего родителя всех элементов ввода
+document.addEventListener("change", function (event) {
+  if (event.target.tagName === "INPUT") {
+    changeInput.call(event.target);
+  }
+});
